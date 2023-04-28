59c810c050590310e87bedcd04964dee5f5d2415
David Avery <avery.di@gmail.com>
2023-04-28 22:02:23 +0000

    Jumbotron Edits

--- src/js/Jumbotron.js
+++ src/js/Jumbotron.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Jumbotron(){
-    return <div className="jumbotron jumbotron-fluid">
-        <div className="container">
-            <h1 className="display-4">Hello, world!</h1>
+    return <div className="jumbotron-fluid py-5">
+        <div className="container-fluid">
+            <h1 className="jumbo-title">Welcome to Middle Earth</h1>
             <p className="lead">Is Lord of the Rings the best trilogy ever made?</p>
             <hr className="my-4"></hr>
             <p>“It’s a dangerous business, Frodo, going out your door. You step onto the road, and if you don’t keep our feet, there’s no knowing where you might be swept off to.” –Bilbo Baggins</p>
